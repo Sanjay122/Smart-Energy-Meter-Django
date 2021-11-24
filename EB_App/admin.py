a570812eed983d3633b6284f07c90ad79ea47a2c
Sanjay M <76098913+Sanjay122@users.noreply.github.com>
2021-11-24 20:13:29 +0530

    Consumer table created
    address, eb number added

--- EB_App/admin.py
+++ EB_App/admin.py
@@ -4,3 +4,4 @@ from . import models
 # Register your models here.
 
 admin.site.register(models.Account)
+admin.site.register(models.Consumer)
--- EB_App/models.py
+++ EB_App/models.py
@@ -1,5 +1,6 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+
 
 # Create your models here.
 
@@ -10,3 +11,23 @@ class Account(models.Model):
     role = models.CharField(max_length=120)
     authorities = models.CharField(max_length=120)
     isEnabled = models.BooleanField()
+
+    def __str__(self):
+        return self.user.username + '  ' + self.role
+
+
+class Consumer(models.Model):
+    account = models.OneToOneField(Account, on_delete=models.CASCADE)
+    supply_phase = models.IntegerField()
+    door_number = models.CharField(max_length=120, null=True, blank=True)
+    street = models.CharField(max_length=120, null=True, blank=True)
+    city = models.CharField(max_length=120)
+    post_office = models.CharField(max_length=120)
+    taluk = models.CharField(max_length=120)
+    district = models.CharField(max_length=120)
+    state = models.CharField(max_length=120)
+    pin_number = models.IntegerField()
+    eb_number = models.CharField(max_length=14)
+
+    def __str__(self):
+        return self.account.user.username
